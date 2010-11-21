2c343fe48fe48d4cf9a04208363b09533cc98455
Prashant Somashekar <prashsomash@gmail.com>
2010-11-20 23:38:45 -0500

    Revert "sched: Fix TASK_WAKING vs fork deadlock"
    This reverts commit 6d94134f5f3f8bede26d4f700e17154d590d6d6e.

--- kernel/sched_fair.c
+++ kernel/sched_fair.c
@@ -1411,8 +1411,7 @@ static int select_idle_sibling(struct task_struct *p, int target)
  *
  * preempt must be disabled.
  */
-static int
-select_task_rq_fair(struct rq *rq, struct task_struct *p, int sd_flag, int wake_flags)
+static int select_task_rq_fair(struct task_struct *p, int sd_flag, int wake_flags)
 {
 	struct sched_domain *tmp, *affine_sd = NULL, *sd = NULL;
 	int cpu = smp_processor_id();
@@ -1486,11 +1485,8 @@ select_task_rq_fair(struct rq *rq, struct task_struct *p, int sd_flag, int wake_
 		if (affine_sd && (!tmp || affine_sd->span_weight > sd->span_weight))
 			tmp = affine_sd;
 
-		if (tmp) {
-			spin_unlock(&rq->lock);
+		if (tmp)
 			update_shares(tmp);
-			spin_lock(&rq->lock);
-		}
 	}
 #endif
 
