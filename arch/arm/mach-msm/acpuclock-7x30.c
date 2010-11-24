2d3e4726732e4f6e5c7c93c9fdfeb1c4810b35e9
Prashant Somashekar <prashsomash@gmail.com>
2010-11-23 20:33:44 -0500

    increase low end by .25

--- arch/arm/mach-msm/acpuclock-7x30.c
+++ arch/arm/mach-msm/acpuclock-7x30.c
@@ -129,12 +129,12 @@ static struct cpufreq_frequency_table freq_table[] = {
 #define SRC_AXI  (-1)
 static struct clkctl_acpu_speed acpu_freq_tbl[] = {
         { 24576,  SRC_LPXO, 0, 0,  30720,  850, VDD_RAW(850) },
-        { 61440,  PLL_3,    5, 11, 61440,  875, VDD_RAW(875) },
-        { 122880, PLL_3,    5, 5,  61440,  900, VDD_RAW(900) },
-        { 184320, PLL_3,    5, 4,  61440,  925, VDD_RAW(925) },
-        { MAX_AXI_KHZ, SRC_AXI, 1, 0, 61440, 950, VDD_RAW(950) },
-        { 245760, PLL_3,    5, 2,  61440,  950, VDD_RAW(950) },
-        { 368640, PLL_3,    5, 1,  122800, 950, VDD_RAW(950) },
+        { 61440,  PLL_3,    5, 11, 61440,  900, VDD_RAW(900) },
+        { 122880, PLL_3,    5, 5,  61440,  950, VDD_RAW(950) },
+        { 184320, PLL_3,    5, 4,  61440,  950, VDD_RAW(950) },
+        { MAX_AXI_KHZ, SRC_AXI, 1, 0, 61440, 975, VDD_RAW(975) },
+        { 245760, PLL_3,    5, 2,  61440,  975, VDD_RAW(975) },
+        { 368640, PLL_3,    5, 1,  122800, 975, VDD_RAW(975) },
         { 768000, PLL_1,    2, 0,  153600, 975, VDD_RAW(975) },
 	/* Make sure any freq based from PLL_2 is a multiple of 19200! */
         { 806400, PLL_2,    3, 0,  192000, 975, VDD_RAW(975) },
